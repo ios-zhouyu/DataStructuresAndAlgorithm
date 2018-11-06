a3d9d3fdd5c9ffbe7da0cd4a47e6a26003cecdc9
zhouyu <1512450002@qq.com>
2018-11-06 15:06:40 +0800

    链表的初始化,插入和遍历

--- DataStructuresDemo/008-基本链表的创建/main.c
+++ DataStructuresDemo/008-基本链表的创建/main.c
@@ -10,11 +10,22 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+//原因是因为在c的结构体中，字符串不能直接这样赋值，需要用到string中的拷贝语句。
+stu->num = 0001;
+strcpy(stu->name, "zhouyu");
+strcpy(stu->address, "北京市海淀区");
+
+Node node = {0002, "zhangsan","上海市黄浦区"};
+stu = &node;
+ */
+
 //定义结构体
 struct List {
     int num;
-    int age;
-    char *name;
+    char name[20];
+    char address[200];
+    struct List *next;
 };
 
 //定义新类型
@@ -22,15 +33,60 @@ typedef struct List Node;
 //定义新类型指针
 typedef Node * LinkList;
 
+//初始化带头结点的链表
+LinkList initializeList() {
+    LinkList head;
+    head = (LinkList)malloc(sizeof(Node));
+    if (!head) {
+        printf("初始化head失败");
+        exit(1);
+    }
+    strcpy(head->name, "这是头结点");
+    head->next = NULL;
+    return head;
+}
+
+//尾插法创建循环链表
+LinkList createListFromTail(LinkList head) {
+    LinkList newNode;
+    LinkList temp = head;
+    for (int i = 0; i < 20; i++) {
+        //初始化新结点并赋值
+        newNode = (LinkList)malloc(sizeof(Node));
+        if (!newNode) {
+            printf("创建新结点失败");
+            exit(1);
+        }
+        newNode->num = i + 1000;
+        strcpy(newNode->name, "zhouyu");
+        newNode->next = NULL;
+        
+        //将新结点链接到表中,并移动到下一个位置
+        temp->next = newNode;
+        temp = newNode;
+    }
+    return head;
+}
+
+//遍历链表
+void loopThroughLink(LinkList head) {
+    LinkList temp = head;
+    while (temp != NULL) {
+        printf("%d -- %s \n", temp->num, temp->name);
+        temp = temp->next;
+    }
+}
+
 int main(int argc, const char * argv[]) {
     
-    LinkList stu;
-    stu = (LinkList)malloc(sizeof(Node));
+    //初始化头结点
+    LinkList head = initializeList();
     
-    stu->name = "zhouyu";
-    stu->num = 123456;
-    stu->age = 27;
+    //指向链表头部的移动指针来创建链表
+    LinkList ptr = createListFromTail(head);
+
+    //遍历链表
+    loopThroughLink(ptr);
     
-    printf("Hello, World!\n");
     return 0;
 }
