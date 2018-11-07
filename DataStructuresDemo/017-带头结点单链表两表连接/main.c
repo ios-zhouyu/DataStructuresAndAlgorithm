7b3781adcb6a8646a968dab504d14f1b734d3e2e
zhouyu <1512450002@qq.com>
2018-11-07 18:36:13 +0800

    单链表两表连接

--- DataStructuresDemo/017-带头结点单链表两表连接/main.c
+++ DataStructuresDemo/017-带头结点单链表两表连接/main.c
@@ -7,9 +7,70 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_SIZE 10
+
+static int listNum = 1000;
+
+struct List {
+    int num;
+    struct List *next;
+};
+typedef struct List Node;
+typedef Node * LinkList;
+
+LinkList initialList() {
+    LinkList head = (LinkList)malloc(sizeof(Node));
+    if (!head) {
+        printf("initial head fatiled\n");
+        exit(1);
+    }
+    head->num = 0;
+    head->next = NULL;
+    return head;
+}
+
+LinkList createNode() {
+    LinkList node = (LinkList)malloc(sizeof(Node));
+    if (!node) {
+        printf("create new node fatiled\n");
+        exit(1);
+    }
+    node->next = NULL;
+    return node;
+}
+
+LinkList createList(LinkList head) {
+    LinkList temp = head;
+    for (int i = 0; i < MAX_SIZE; i++) {
+        LinkList node = createNode();
+        node->num = i + listNum;
+        
+        temp->next = node;
+        temp = node;
+    }
+    listNum *= 2;
+    return head;
+}
+
+void loopList(LinkList list) {
+    LinkList temp = list;
+    while (temp != NULL) {
+        printf("%d\n", temp->num);
+        temp = temp->next;
+    }
+}
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    
+    LinkList head1 = initialList();
+    LinkList list1 = createList(head1);
+    LinkList head2 = initialList();
+    LinkList list2 = createList(head2);
+    loopList(list1);
+    printf("------------------------------------\n");
+    loopList(list2);
     return 0;
 }
