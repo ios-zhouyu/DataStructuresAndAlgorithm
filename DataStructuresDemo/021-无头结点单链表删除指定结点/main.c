4ed17cfb4f6515bfc5f9cafce3528165c4ec96a8
zhouyu <1512450002@qq.com>
2018-11-08 17:28:59 +0800

    无头结点链表删除指定结点

---
+++ DataStructuresDemo/021-无头结点单链表删除指定结点/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  021-无头结点单链表删除指定结点
+//
+//  Created by zhouyu on 2018/11/8.
+//  Copyright © 2018年 zhouyu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
